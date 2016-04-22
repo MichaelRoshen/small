9e6f9dd5d22c1f297d9e814852e4cdf3f95c0402
超子 <cc90146@alibaba-inc.com>
2016-04-22 13:06:16 +0800

    api_v1

---
+++ config/application.rb
@@ -0,0 +1,28 @@
+require File.expand_path('../boot', __FILE__)
+
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "sprockets/railtie"
+# require "rails/test_unit/railtie"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Small
+  class Application < Rails::Application
+    config.middleware.insert_before 'Rack::Runtime', 'Rack::Cors' do
+      allow do
+        origins '*'
+        resource '*',
+                 headers: :any,
+                 methods: [:get, :put, :post, :patch, :delete, :options]
+      end
+    end
+    config.active_record.raise_in_transactional_callbacks = true
+  end
+end
